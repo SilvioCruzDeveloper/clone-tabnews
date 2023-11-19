c50969f8db42f8ac86186e68087156fa68794866
SilvioCruzDeveloper <137232326+SilvioCruzDeveloper@users.noreply.github.com>
2023-11-19 03:27:47 -0300

    foi

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
 // pages/index.js (assumindo uso do Next.js)
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return (
-        <div style={
-            {
-                textAlign: 'center'
-            }
-        }>
-            <h1>to indo bem?</h1>
-            <p>oi ny , te amo muito sabias?</p>
-            
-        </div>
-       
-    );
+  return (
+    <div
+      style={{
+        textAlign: "center",
+      }}
+    >
+      <h1>to indo bem?</h1>
+      <p>oi ny , te amo muito sabias?</p>
+    </div>
+  );
 }
 
 export default Home;

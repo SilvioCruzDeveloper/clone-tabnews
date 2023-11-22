bc3a8fc4e4d5bef091bd3706341417cdac668253
SilvioCruzDeveloper <137232326+SilvioCruzDeveloper@users.noreply.github.com>
2023-11-22 02:13:14 -0300

    atializando meu amor
    amooooorr

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,47 @@
-// pages/index.js (assumindo uso do Next.js)
-import React from "react";
+// pages/index.js
 
-function Home() {
+import React, { useState } from "react";
+
+const Home = () => {
+  const [mensagemAmor, setMensagemAmor] = useState("te amo muito sabias?");
+
+  const styles = {
+    container: {
+      textAlign: "center",
+      margin: "20px",
+    },
+    title: {
+      color: "#ff66b2", // Rosa
+    },
+    message: {
+      color: "#ff3399", // Rosa mais escuro
+      fontSize: "1.2em",
+      marginBottom: "20px",
+    },
+    button: {
+      backgroundColor: "#ff66b2", // Rosa
+      color: "#fff", // Texto branco
+      fontSize: "1em",
+      padding: "10px 20px",
+      border: "none",
+      borderRadius: "5px",
+      cursor: "pointer",
+      transition: "background-color 0.3s ease", // Animação de transição de cor de fundo
+    },
+  };
+
   return (
-    <div
-      style={{
-        textAlign: "center",
-      }}
-    >
-      <h1>to indo bem?</h1>
-      <p>oi ny , te amo muito sabias?</p>
-      <P>te amo te amoooo!!!!!</P>
+    <div style={styles.container}>
+      <h1 style={styles.title}>to indo bem?</h1>
+      <p style={styles.message}>oi ny, {mensagemAmor}</p>
+      <button
+        style={styles.button}
+        onClick={() => setMensagemAmor("te amo mais ainda!!!!!")}
+      >
+        Expressar Amor
+      </button>
     </div>
   );
-}
+};
 
 export default Home;
